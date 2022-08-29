a5edc405913c30ca540339e2621a3bd2e76f9b39
Nathan Griffiths <49567130+vs-postmedia@users.noreply.github.com>
2022-08-29 10:20:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+Includes normalize.css v8.0.1 from HTML5 boilerplate v7.1.0.
+
+EMBED
+`<iframe src="[URL_GOES HERE]" style="width:100%;height:500px;" scrolling="yes"></iframe>`
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = (props) => {
+	return (
+		<footer>
+			<p className="source">Source:  <a href="https:vancouversun.com" target="_blank">TK</a></p>
+			<p className="source">NOTE: tk.</p>
+		</footer>
+	)
+};
+
+export default Footer;
+
